b739cb945b0531eca04336968c51a818eaa6f47f
Satyam <satyam8233kuar@gmail.com>
2026-08-26 20:14:26 +0530

    Refactor ALU control logic for clarity and structure

--- rtl/alu_control.v
+++ rtl/alu_control.v
@@ -12,37 +12,31 @@ localparam ALU_OR  = 4'b0001;
 localparam ALU_ADD = 4'b0010;
 localparam ALU_SUB = 4'b0110;
 always@(*) 
-begin 
-    case({aluop,fun7,fun3})
+   case (aluop)
 
-{ALUOP_LS,1'b0,3'b000} :
-    control_out = ALU_ADD;
+    ALUOP_RTYPE: begin
+        case ({fun7,fun3})
+            4'b0000: control_out = ALU_ADD;
+            4'b1000: control_out = ALU_SUB;
+            4'b0111: control_out = ALU_AND;
+            4'b0110: control_out = ALU_OR;
+        endcase
+    end
 
-{ALUOP_BRANCH,1'b0,3'b000} :
-    control_out = ALU_SUB;
+    ALUOP_ITYPE: begin
+        case (fun3)
+            3'b000: control_out = ALU_ADD;
+            3'b110: control_out = ALU_OR;
+            3'b111: control_out = ALU_AND;
+        endcase
+    end
 
-{ALUOP_RTYPE,1'b0,3'b000} :
-    control_out = ALU_ADD;
+    ALUOP_LS:
+        control_out = ALU_ADD;
 
-{ALUOP_RTYPE,1'b1,3'b000} :
-    control_out = ALU_SUB;
+    ALUOP_BRANCH:
+        control_out = ALU_SUB;
 
-{ALUOP_RTYPE,1'b0,3'b111} :
-    control_out = ALU_AND;
-
-{ALUOP_RTYPE,1'b0,3'b110} :
-    control_out = ALU_OR; 
-    
-{ALUOP_ITYPE,1'b0,3'b000} :
-    control_out = ALU_ADD;
-
-
-{ALUOP_ITYPE,1'b0,3'b110} :
-    control_out = ALU_OR;
-
-
-{ALUOP_ITYPE,1'b0,3'b111} :
-    control_out = ALU_AND;
-    endcase
+endcase
 end
 endmodule 
